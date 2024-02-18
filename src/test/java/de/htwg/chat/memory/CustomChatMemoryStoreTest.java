46c683ec1edad70a5d5005269b221c64dbcc8985
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2024-02-18 11:03:33 +0100

    weiteren Test hinzugefügt

--- src/test/java/de/htwg/chat/memory/CustomChatMemoryStoreTest.java
+++ src/test/java/de/htwg/chat/memory/CustomChatMemoryStoreTest.java
@@ -2,10 +2,13 @@ package de.htwg.chat.memory;
 
 import de.htwg.chat.entities.Conversation;
 import de.htwg.chat.entities.Message;
+import de.htwg.chat.entities.SystemPrompt;
 import de.htwg.chat.repositories.AnswerRepository;
 import de.htwg.chat.repositories.ConversationRepository;
 import de.htwg.chat.repositories.MessageRepository;
 import de.htwg.chat.repositories.SystemPromptRepository;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.UserMessage;
 import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
@@ -93,6 +96,28 @@ class CustomChatMemoryStoreTest {
         verify(messageRepository, times(1)).persist(any(Message.class));
         //expecting 0 messages because the db is mocked
         assertEquals(0, customChatMemoryStore.getMessages(conversation.getId().toString()).size());
+    }
+
+    @Test
+    void testUpdatedMessagesForListOfOneAndTwoMessageInTheConversationWithSystemMessage() {
+        Conversation conversation = new Conversation();
+        conversation.setId(java.util.UUID.randomUUID());
+
+        doNothing().when(conversationRepository).persist(conversation);
+        when(conversationRepository.findById(conversation.getId())).thenReturn(conversation);
+        when(messageRepository.findByConversationId(any(UUID.class))).thenReturn(List.of(
+                new Message.MessageBuilder().message("test").conversation(conversation).build()));
+        when(systemPromptRepository.findByConversationId(any(UUID.class))).thenReturn(new SystemPrompt("test"));
+
+        customChatMemoryStore.updateMessages(conversation.getId().toString(), List.of(new UserMessage("test,test")));
+        verify(messageRepository, times(1)).persist(any(Message.class));
+        verify(systemPromptRepository, times(0)).persist(any(SystemPrompt.class));
+
+        System.out.println(customChatMemoryStore.getMessages(conversation.getId().toString()));
+        List<ChatMessage> messages = customChatMemoryStore.getMessages(conversation.getId().toString());
+
+        assertEquals(2, messages.size());
+        assertEquals(SystemMessage.class, messages.get(0).getClass());
     }
 
 }
