1bf9018eef20e33b6d1a94dbb6ff29959f3e6624
Sascha Kiebler <sascha.kiebler@htwg-konstanz.de>
2024-03-02 19:01:48 +0100

    embeddings werden jz mit den Dokumenten gelöscht dank trigger in der db

---
+++ src/main/resources/db/migration/Create_Trigger_for_FileUpload_Embedding_Delete.sql
@@ -0,0 +1,22 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+-- noinspection SqlDialectInspectionForFile
+
+CREATE FUNCTION delete_embeddings_with_file() RETURNS trigger
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+DELETE FROM embeddings
+WHERE metadata ->> 'fileKey' = concat(OLD.id,'');
+RETURN OLD;
+END;
+$$;
+
+CREATE TRIGGER trigger_delete_embeddings_with_file
+    BEFORE DELETE
+    ON uploadedfile
+    FOR EACH ROW
+    EXECUTE PROCEDURE delete_embeddings_with_file();
+
+ALTER FUNCTION delete_embeddings_with_file() OWNER TO gibler;
